14c2c8d08bbd361df0eb16dc3597b5ba9f678eca
Vijay <vijay.thorat@biddano.com>
2024-11-09 14:49:42 +0530

    added gunicorn code

---
+++ scripts/python_dependencies.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+virtualenv /home/ubuntu/env
+source /home/ubuntu/env/bin/activate
+
+pip install -r /home/ubuntu/home_expenses/requirements.txt
+sudo apt-get install -y libpq-dev
+pip install psycopg2
+cd /home/ubuntu/env/bin
+pip install gunicorn
