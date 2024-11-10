763381b0a2b7a7788c128eb85956cbef864e63ae
Vijay <vijay.thorat@biddano.com>
2024-11-10 13:38:22 +0530

    added models migraions

--- scripts/migrate.sh
+++ scripts/migrate.sh
@@ -3,3 +3,6 @@ source /home/ubuntu/env/bin/activate
 cd /home/ubuntu/home_expenses
 python manage.py makemigrations
 python manage.py migrate
+
+sudo service gunicorn restart 
+sudo service nginx restart
