7e211e8ae036cf6700d4c9e49b9f2ed6c782222d
Vijay <vijay.thorat@biddano.com>
2024-11-10 15:51:49 +0530

    updated filter version

--- scripts/migrate.sh
+++ scripts/migrate.sh
@@ -2,17 +2,19 @@
 source /home/ubuntu/env/bin/activate
 cd /home/ubuntu/home_expenses
 
+python manage.py makemigrations
+python manage.py migrate
+
 cp /home/ubuntu/db.sqlite3 .
 chmod 664 db.sqlite3
 sudo chown -R ubuntu:ubuntu /home/ubuntu/home_expenses
 chmod 775 /home/ubuntu/home_expenses
-python manage.py makemigrations
-python manage.py migrate
 cd home_expenses/
 cp /home/ubuntu/.env .
 cd ..//..//
 sudo rm -r .env
 sudo rm -r db.sqlite3
+
 sudo service gunicorn restart 
 sudo service nginx restart
 
