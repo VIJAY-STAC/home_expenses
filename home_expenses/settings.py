76ebc671a4652ef1b537ede5f2e1b351102ce907
Vijay <vijay.thorat@biddano.com>
2024-11-11 01:30:03 +0530

    removed note field

--- home_expenses/settings.py
+++ home_expenses/settings.py
@@ -79,10 +79,17 @@ WSGI_APPLICATION = 'home_expenses.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': '/home/ubuntu/db.sqlite3',  # Absolute path to db.sqlite3
     }
 }
 
